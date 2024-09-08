4ec3147e96e248f6189d5696d1f48b009f2f64ed
vlimap <valtemir.procopio@gmail.com>
2024-09-08 19:03:40 -0300

    fix: button link

--- scripts.js
+++ scripts.js
@@ -47,12 +47,11 @@ function renderBooks(books) {
                 <p><strong>Autor:</strong> ${book.author}</p>
                 <p><strong>Ano:</strong> ${book.year}</p>
                 <p><strong>Descrição:</strong> ${book.description}</p>
-                <button class="download-btn">
-                <a aria-label="Abrir ${book.title}" href="${book.url}" target="_blank">
+                
+                <a class="download-btn" aria-label="Abrir ${book.title}" href="${book.url}" target="_blank">
                     <img src="./assets/icons/download.svg" alt="Ícone de download" class="download-icon">
                     Abrir
                 </a>
-                </button>
             </div>
         `;
 
